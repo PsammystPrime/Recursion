e19bf74f5bc9d9a46cd0146db34a48d9be00ead8
PsammystPrime <samuelprime384@gmail.com>
2024-01-08 20:14:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,35 +1,31 @@
-function fibs(num){
-    
+//Creating a fibonacci sequence using loop method
+function fibs(num){  
     let previousNum = 0, nextNum = 1, sum = 0;
     
     for (let index = 0; index < num; index++) {
-
       sum = previousNum + nextNum;
       nextNum = previousNum;
       previousNum = sum;
 
-        console.log(nextNum)
-        
-    }console.log('hoo')
+      console.log(nextNum)        
+    }
+    console.log('finished looping')
 };
-fibs(5);
 
+//Creating the same fibonacci sequence using a recursive function
 let previousNum = 0, nextNum = 1, sum = 0, index = 0;  
 function fibsRec(num) {
     
     if (index >= num){
-        console.log('haa')
-  
+        console.log('finished recursing')
     } else{
-      console.log(previousNum);
-
+        console.log(previousNum);
         sum = previousNum + nextNum;
         previousNum = nextNum;
         nextNum = sum;
         index++;
+
         fibsRec(num);        
     }
-
 };
-fibsRec(5)
 
