55d77a6abb5841ad0d47e98e70057b1928ec80d1
PsammystPrime <samuelprime384@gmail.com>
2024-01-14 17:51:12 +0300

    Implement Sort Logic

--- index.js
+++ index.js
@@ -39,3 +39,39 @@ function Sort() {
     console.log(secondArray)
 };
 
+//sorting the same array using the mergesort method
+function mergeSort() {
+    const array = [3, 2, 1, 13, 8, 5, 0, 1] 
+   // Divide array by half and sort them each
+    const arrayLength = array.length/2; //finding the centre of the array
+    const leftArr = array.splice(0, arrayLength);
+    const divide = array.length/2 ; // Divide the smaleer arrays by half
+    const leftLeftArr = leftArr.splice(0, divide).sort(); //Get the left arr of the left part
+
+    const rightArr = array.sort()
+    const rightLeftArr = rightArr.splice(0, divide); //Get the left arr of the right part
+
+    //Merge Arrays
+    const merger = []
+    const merge = mergeSort(leftArr, leftLeftArr)
+
+    //Get the right half array
+    //  const array3 = array.sort((a,b)=>{
+    //     return
+    //  })
+    // //Get the half of the left half
+    // 
+    // .sort() //sorted
+    // //Get the right half of the left half
+    // const rightLeftArr = leftArr //Sorted
+    //     //sort the right half of the array
+
+        
+    console.log(merge)
+    console.log(rightLeftArr)
+    console.log(leftLeftArr)
+}
+// mergeSort()
+
+
+
